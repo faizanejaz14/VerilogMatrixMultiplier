7856770e3bc5413ccdfc44782e38836247d948a2
Irtaza Hyder <irtazahyder2002@gmail.com>
2024-12-25 16:16:22 +0500

    10x10 Multiplier working

--- FPGA_serial_comm.py
+++ FPGA_serial_comm.py
@@ -1,15 +1,36 @@
 import serial
 import time
+import numpy as np
 
-ser = serial.Serial('COM7', 9600)
-ser.write(b'a')
-time.sleep(2)
+def complete_uart(mat_size):
+    ser = serial.Serial('COM3', 9600)
 
-ser.write(b'b')
-time.sleep(2)
+    matrixA = np.random.randint(10, size=mat_size**2)
+    print(matrixA)
 
-ser.write(b'c')
-time.sleep(2)
+    for num in matrixA:
+        time.sleep(0.01)
+        ser.write(chr(num).encode())
+        ser.flush()
 
-ser.write(b'd')
-ser.close()
+    x = input("Waiting for switch to be flipped!!")
+    matrixB = np.random.randint(10, size=mat_size**2)
+    print(matrixB)
+               
+    for num1 in matrixB:
+        time.sleep(0.01)
+        ser.write(chr(num1).encode())
+        ser.flush()
+
+    counter = 0
+    FPGA_mat = []
+    while counter < mat_size**2:
+        a = ser.read()
+        FPGA_mat.append(int.from_bytes(a))
+        counter += 1
+    ser.close()
+
+    matrixA = np.array(matrixA).reshape(mat_size, mat_size)
+    matrixB = np.array(matrixB).reshape(mat_size, mat_size)
+    print("FPGA Output: ", np.array(FPGA_mat).reshape(mat_size, mat_size))
+    print("Actual Output: ", np.matmul(matrixA, matrixB))
--- memory.v
+++ memory.v
@@ -21,8 +21,8 @@
 //-------------------------------------- memory --------------------------------------//
 
 module memory(input clk, rst, write, read,
-              input [5:0] write_address,
-              input [5:0] read_address,
+              input [31:0] write_address,
+              input [31:0] read_address,
               input [size - 1:0] write_value,
               output reg [size - 1:0] data);
   parameter row = 2;
@@ -38,12 +38,12 @@ module memory(input clk, rst, write, read,
   
   
   //// zakria addition
-  //initial begin
+  initial begin
   //initialize
-  //for (i = 0; i < row; i = i+1)
-  //      for (j = 0; j < column; j = j + 1)
-  //        mem[row*i + j] = 5-2*i + j; // make 0 
-  //end
+  for (i = 0; i < row; i = i + 1)
+        for (j = 0; j < column; j = j + 1)
+          mem[row*i + j] = 0; // make 0 
+  end
   //// zakria close
   
   //always @ (posedge clk) begin
@@ -55,15 +55,13 @@ module memory(input clk, rst, write, read,
   always @ (posedge clk) begin
     // clearing memory
     if (rst) begin
-	//	for (i = 0; i < row; i = i+1)
-   //     for (j = 0; j < column; j = j + 1)
-   //       mem[row*i + j] <= 5 + (10*i)+j;
-	mem[0] <= 8'd0;
-	mem[1] <= 8'd85;
-	mem[2] <= 8'd1;
-	mem[3] <= 8'd170;
-
-
+		for (i = 0; i < row; i = i+1)
+        for (j = 0; j < column; j = j + 1)
+          mem[row*i + j] <= 0;
+	//mem[0] <= 8'd0;
+	//mem[1] <= 8'd8;
+	//mem[2] <= 8'd1;
+	//mem[3] <= 8'd9;
     end
     // writing in memory
     else if (write) mem[write_address] <= write_value;
